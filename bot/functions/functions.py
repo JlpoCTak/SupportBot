3641753b65cdcbc9b6ee4f3a211ae9547758a5dd
JlpoCTak <JlpocTaK@yandex.ru>
2025-04-28 16:17:49 +0300

    .

--- bot/functions/functions.py
+++ bot/functions/functions.py
@@ -1,4 +1,9 @@
+import time
+import asyncio
+
 from loader import db_question, db_answer
+from time import sleep
+from loader import bot
 
 
 async def check_answer(user_id, msg_id):
@@ -10,3 +15,16 @@ async def check_answer(user_id, msg_id):
             return None
         else:
             return data
+
+
+async def check_unanswer_question():    #проверка неотвеченных вопросов раз в 5 мин
+    while True:
+        await asyncio.sleep(300)
+        with db_question as connection:
+            cursor = connection.cursor()
+            data = cursor.execute('Select * From Questions')
+            if data:
+                active_sups = cursor.execute('Select user_id From active_supports')
+                for support in active_sups:
+                    await bot.send_message(chat_id=support[0], text='Появились вопросы')
+
--- main.py
+++ main.py
@@ -10,12 +10,15 @@ import bot.handlers.admins.admins as admins
 import bot.handlers.users.users as users
 import bot.handlers.supports.supports as supports
 
+from bot.functions.functions import check_unanswer_question
+
 
 async def main() -> None:
     """
     Главная функция запускающая бота
     """
     dp.include_routers(admins.router, users.router, supports.router)
+    task = asyncio.create_task(check_unanswer_question())
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
